279b9bf18caacd0ca6f4a0797225770f2557efc5
kesh6190 <skchundunsing@gmail.com>
2021-03-26 21:57:31 +0400

    first commit

---
+++ stats.php
@@ -0,0 +1,16 @@
+<?php
+	$x=10.0;
+	$y=-4;
+	echo "Simple calculation stuff...".PHP_EOL;
+	echo "".PHP_EOL;
+	echo "x=".$x.PHP_EOL;
+	echo "y=".$y.PHP_EOL;
+	echo "---------------------------".PHP_EOL;
+	echo "x+y=".($x+$y).PHP_EOL;
+    	echo "x-y=".($x-$y).PHP_EOL;
+    	echo "x*y=".($x*$y).PHP_EOL;
+    	echo "x**y=".($x**$y).PHP_EOL;
+    	echo "x/y=".($x/$y).PHP_EOL;
+    	echo "x%y=".($x%$y).PHP_EOL;
+?>
+
